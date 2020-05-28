61bd5ae0a4cdad5fae4581a03abef0836c382f3b
Rakesh Khot <rakeshkhot18@gmail.com>
2020-05-28 17:30:32 +0530

    code added

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets_cidr" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "public_subnets_cidr" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "azs" {
+  type    = list
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+
+variable "access" {
+
+}
+
+variable "secret" {
+
+}
